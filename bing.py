9e955ef47ad4fa10ce5e817a83dc0cab0b86abcd
tkk7406 <tkk7406@hotmail.com>
2020-07-31 13:53:54 +0300

    Add image info/date

--- bing.py
+++ bing.py
@@ -2,32 +2,41 @@ from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup as beausoup
 from datetime import datetime
 import pytz
+import os.path
+import piexif
 
 
-def main():    
-    urlretrieve(getImage(), "C:/Users/Aristo/Desktop/Coding Projects/Python Web Scraping/" + getDate() + ".jpg")
-    print("main")
+def main():
+    imgPath = "C:/Users/Aristo/Downloads/BingWallpaper-" + getDate() + ".jpg"
+    if os.path.isfile(imgPath) == False:
+        urlretrieve(getImage(), imgPath)
+        imgInfo = {270: getDescription(), 315: getAuthor()}
+        imgData = piexif.dump({"0th":imgInfo})
+        piexif.insert(imgData, imgPath)
 
 def getImage():
+    imgDiv = parsePage().select_one("div.img_cont")
+    imgLink = "https://bing.com" + imgDiv['style'].split("url(")[1].split(")")[0]
+    return imgLink
+
+def getDescription():
+    descDiv = parsePage().select_one("a.title")
+    return descDiv.text
+
+def getAuthor():
+    authDiv = parsePage().select_one("div.copyright")
+    return authDiv.text.split("© ")[1]
+
+def parsePage():
     source = "https://www.bing.com/?cc=gb"
     client = urlopen(source)
-    print("client")
     page = client.read()
-    print("read")
     client.close()
-    parsed = beausoup(page, "html.parser")
-    print("parsed")
-    imageDiv = parsed.select_one("div.img_cont")
-    imageLink = "https://bing.com" + imageDiv['style'].split("url(")[1].split(")")[0]
-    print("saved")
-    return imageLink
+    return beausoup(page, "html.parser")
 
 def getDate():
     uk = pytz.timezone('Europe/London')
-    date = str(datetime.now(uk).year()) + "-" + str(datetime.now(uk).month()) + "-" + str(datetime.now(uk).day())
-    print(datetime.now(uk).date())
-    return date
+    return str(datetime.now(uk).date())
 
 if __name__ == "__main__":
-    print("start")
     main()
